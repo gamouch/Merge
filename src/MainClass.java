da40b3c32d852a769071b873189a080afe8d61cd
Oussama Gmach <gmachoussama@gmail.com>
2016-01-15 14:12:30 +0100

    Other commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,14 +3,14 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		int data[]= {-1,2, 42, 7, 14, 6};
+		int data[]= {2, 42, 7, 14, 6};
 		
 		System.out.println("First Test.");
-		for (int i : data) {
-			
+		for (int i = 0; i < data.length; i++) {
 			System.out.println("Data n° "+i+ ": " +data[i]);
 		}
+						
+		}
 
-	}
 
 }
