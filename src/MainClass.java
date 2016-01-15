1ff79e266c06548713b6cb3757c6410c69f81f71
Oussama Gmach <gmachoussama@gmail.com>
2016-01-15 13:50:56 +0100

    Other commit test

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,7 +3,13 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
+		int data[]= {-1,2, 42, 7, 14, 6};
+		
 		System.out.println("First Test.");
+		for (int i : data) {
+			
+			System.out.println("Data n° "+i+ ": " +data[i]);
+		}
 
 	}
 
