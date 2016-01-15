389c2348fb87002cf6d7d1563ff79b727e661fda
Oussama Gmach <gmachoussama@gmail.com>
2016-01-15 16:03:46 +0100

    Initial test

--- src/MainClass.java
+++ src/MainClass.java
@@ -10,10 +10,10 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		int data[]= {2, 42, 7, 14, 6};
+		int data[]= {2, 42, 7, 14, 6,8 ,12,32};
 		//My second test
-		System.out.println("Third Test.");
-
+		System.out.println("Initial Test.");
+		System.out.println("Merge Test.");
 		for (int i = 0; i < data.length; i++) {
 			System.out.println("Data n "+i+ ": " +data[i]);
 			
