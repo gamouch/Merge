3c86089327860ec091721d3f177a9303f2b61f26
Salah Gmach <sassouma2010@gmail.com>
2016-01-15 14:17:24 +0100

    Default commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,8 +3,16 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
+		int data[]= {2, 42, 7, 14, 6};
+		
 		System.out.println("First Test.");
+		int sum = 0;
+		for (int i = 0; i < data.length; i++) {
+			System.out.println("Data n° "+i+ ": " +data[i]);
+			sum += i;
+		}
+		System.out.println("La somme est :" +sum);				
+		}
 
-	}
 
 }
