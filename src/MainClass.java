68afe398e9b22504cd3e33ff440a8375f56f46d4
Oussama Gmach <gmachoussama@gmail.com>
2016-02-14 14:27:55 +0100

    Message d'erreur (+1 squashed commits)
    Squashed commits:
    
    [c9c6907] Message commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -20,16 +20,18 @@ public class MainClass {
 		System.out.println("Initial Test.");
 		System.out.println("Merge Test.");
 		int sum = 0;
-		int multip= 0;
 		for (int i = 0; i < data.length; i++) {			
 			System.out.println("Data n "+i+ ": " +data[i]);
 			sum += data[i];
-			multip -= data[i]; 
 		}	
 		// somme
 		System.out.println("la somme est: "+sum);
 		
-		System.out.println("la multiplication est: "+multip);
+		//Message
+		System.out.println("Ceci est message");
+		
+		//Error message
+		 System.err.println("Ceci est un message d'erreur");
 		}
 
 
